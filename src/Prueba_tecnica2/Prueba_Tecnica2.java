2717700b30092f0c185fc5db5924381e4246c166
OzAvilaD <oscar.r.avila.d@gmail.com>
2024-10-07 21:52:29 -0600

    inicio segunda prueba - Sistema de Tickets

---
+++ src/Prueba_tecnica2/Prueba_Tecnica2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prueba_tecnica2;
+
+/**
+ *
+ * @author tracs
+ */
+public class Prueba_Tecnica2 {
+    
+}
